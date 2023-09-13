2d7fe5f76df9b43ad510287c7e0b644552d14f59
Shishir Yalburgi <shishiryalburgi@gmail.com>
2023-09-12 21:39:16 -0500

    Almost final

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,56 +4,49 @@
 #
 # Author 2 : Uthama Kadengodlu          NETID: 
 # This file implements gradiant descent class
-
-import numpy as np
-import pandas as pd 
 import matplotlib.pyplot as plt 
+import pandas as pd 
+import numpy as np
 
 
 
 Training_file = pd.read_csv("https://raw.githubusercontent.com/Raftaar-98/ML-project-1/main/Training_data.csv",skiprows=[0], header = None)
-Training_file = (Training_file - Training_file.mean()/Training_file.std())
+Training_file = (Training_file - Training_file.mean())/Training_file.std()
+Testing_file = pd.read_csv("https://raw.githubusercontent.com/Raftaar-98/ML-project-1/main/Test_Data.csv",skiprows=[0], header = None)
+Testing_file = (Testing_file - Testing_file.mean())/Testing_file.std()
 
 
 
-def parse_file(data_file):
-        Ind_var = data_file.iloc[:,0:4]
-        ones = np.ones([Ind_var.shape[0],1])
-        Ind_var = np.concatenate((ones,Ind_var),axis = 1)
-
-        Dep_var = data_file.iloc[:,4:5].values
-        func = np.zeros([1,5])
+def preprocess_data(data_file):
+        independent_variable = data_file.iloc[:,0:4]
+        dependent_variable = data_file.iloc[:,4:5].values
         
+        independent_variable=np.concatenate(((np.ones([independent_variable.shape[0],1])),independent_variable),axis = 1)
+     
+        func = np.zeros([1,5])
+        return independent_variable,dependent_variable,func
 
-        learning_rate = 0.01
-        iterations = 1000
-        return Ind_var,Dep_var,func,learning_rate,iterations
+def gen_cost_function(ind_var,dep_var,theta):
+       
+        return np.sum(np.power(((ind_var@theta.T)-dep_var),2))/(2 * len(ind_var))
 
-def epsilon(Ind_var,Dep_Var,func):
-         tobesummed = np.power(((Ind_var @ func.T)-Dep_Var),2)
-         return np.sum(tobesummed)/(2 * len(Ind_var))
-
-def gradientDescent(Ind_var,Dep_var,func,iterations,learning_rate):
-        summ = np.zeros(iterations)
+def GD(ind_var, dep_var,theta, iterations, learning_rate):
+        epsilon = np.zeros(iterations)
         for i in range(iterations):
-            func = func - (learning_rate/len(Ind_var)) * np.sum(Ind_var * (Ind_var @ func.T - Dep_var), axis = 0)
-            summ[i] = epsilon(Ind_var,Dep_var,func)
-
-        return func,summ
-            
-
+            theta = theta - (learning_rate/len(ind_var)) * np.sum(ind_var * (ind_var @ theta.T - dep_var),axis = 0)
+            epsilon[i] = gen_cost_function(ind_var,dep_var,theta)
+        return theta,epsilon
 
 if __name__ == "__main__":
-    Ind_var,Dep_var,func,learning_rate,iterations = parse_file(Training_file)
-    print(Ind_var)
-    print(Dep_var)
-    print(func)
-    g,cost = gradientDescent(Ind_var,Dep_var,func,iterations,learning_rate)
-    print(g)
+    ind_var,dep_var,theta = preprocess_data(Training_file)
+    test_ind_var, test_dep_var, test_theta = preprocess_data(Testing_file)
+    learn_rate = 0.05
+    iterations = 5000
+    theta,epsilon = GD(ind_var,dep_var,theta,iterations,learn_rate)
+    print(theta)
+    print(gen_cost_function(test_ind_var,test_dep_var,theta))
+    fig,ax = plt.subplots()
+    ax.plot(np.arange(iterations),epsilon,'r')
 
-    final_error = epsilon(Ind_var,Dep_var,g)
-    print(final_error)
-
-    fig, ax = plt.subplots()
-    ax.plot(np.arange(iterations),cost,'r')
+  
     
